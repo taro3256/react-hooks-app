627da4c4d66f3d78cf0311fa031178ad2f361463
amaisuna <taro30143014@gmail.com>
2019-08-18 06:49:33 +0900

    localstrage

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -7,15 +7,24 @@ import Events from './Events'
 import OperationLogs from './OperationLogs'
 import AppContext from '../contexts/AppContext'
 import reducer from '../reducers'
-import operationLogs from '../reducers/operationLogs';
+
+const APP_KEY = 'appWithRedux'
 
 function App() {
-  const initialState = {
+  // ローカルストレージからstateを取得
+  const appState = localStorage.getItem(APP_KEY)
+  // ローカルストレージにデータがあればそのJSONデータをパースして初期化
+  const initialState = appState ? JSON.parse(appState) : {
     events: [],
     operationLogs: []
   }
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  // stateが変更される度にJSONデータでローカルストレージに保存される
+  useEffect(() => {
+    localStorage.setItem(APP_KEY, JSON.stringify(state))
+  }, [state])
+  
   return (
     <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
